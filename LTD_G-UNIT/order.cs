90c3c0dbfef7a719b620199716960101e248e764
baratoff <johann84bjarni@gmail.com>
2014-12-09 13:34:17 +0100

    lol

--- LTD_G-UNIT/order.cs
+++ LTD_G-UNIT/order.cs
@@ -18,15 +18,6 @@ namespace LTD_G_UNIT
          public int orderid {get; set;}
          public string Employe { get; set; }
          
-
-    //     private static int newID = 1100 ;
-
-    //     public  order()
-    //{
-    //    newID = newID +1;
-    //    orderid = newID;
-
-    //}
     
     }
 }
